9cf51f8cc96d8635cc4e83b06f09d564248ff93d
sudhanshu <sudhanshusoni45@gmail.com>
2023-10-23 01:12:37 +0530

    feat:remove demo star

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import DemoShootingStar from "./pages/demo_shooting_star/DemoShootingStar";
 import Home from "./pages/home/home";
 import { Route, Routes } from "react-router";
 
@@ -8,7 +7,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="star-demo" element={<DemoShootingStar />} />
       </Routes>
     </>
   );
