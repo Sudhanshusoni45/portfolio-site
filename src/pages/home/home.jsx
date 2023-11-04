c0b4ae6e5b99fcc218bd2c436567bc2a5506dc45
sudhanshu <sudhanshusoni45@gmail.com>
2023-11-04 21:48:34 +0530

    fix:scrollbar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useRef } from "react";
 import styles from "./home.module.css";
 import Starback from "starback";
-import useWindowDimensions from "../../hooks/useWindowWidth";
 import ContactSection from "../../components/contact/contactSection/ContactSection";
 import TestimonialsSection from "../../components/testimonials/testimonialsSection/TestimonialsSection";
 import ShootingStars from "../../components/shootingStars/ShootingStars";
 import AboutSection from "../../components/about/AboutSection/AboutSection";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 const Home = () => {
   const { width } = useWindowDimensions();
@@ -26,7 +26,7 @@ const Home = () => {
           starSize: [0, 1],
           speed: [0, 0.3],
           // -17 just to get rid of x scrollbar
-          width: width - 17,
+          width: width > 480 ? width - 17 : width,
           height: 5000,
           starColor: "white",
         });
