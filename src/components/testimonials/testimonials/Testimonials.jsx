6c6cb0a7ba76982821a3f3385b1b9632ca82f23d
sudhanshu <sudhanshusoni45@gmail.com>
2023-11-12 11:50:35 +0530

    feat:anmol testimonial

--- src/components/testimonials/testimonials/Testimonials.jsx
+++ src/components/testimonials/testimonials/Testimonials.jsx
@@ -25,7 +25,7 @@ const Testimonials = () => {
     {
       heading: "Right way!",
       description:
-        "Someone who will take the time and do the things the “Right way”",
+        "Sudhanshu is someone I'd like to collaborate with more. We've worked on a few projects together, and his skills are impressive. The quality of his work is extraordinary, as he puts in effort and ideas as if he's building the project for himself.",
       name: "Anmol singh bhatia",
       image:
         "https://media.licdn.com/dms/image/C5603AQFZieZETKQumQ/profile-displayphoto-shrink_800_800/0/1661393664905?e=1704326400&v=beta&t=kSaezY3MS75oI9iJ_Ii3tXPDyXvXlYVpcCl3DSNKfXY",
