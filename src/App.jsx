456114a7116ffc3baead378603ff6e349e473d6a
sudhanshu <sudhanshusoni45@gmail.com>
2023-11-12 15:32:48 +0530

    feat:vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Home from "./pages/home/home";
 import { Route, Routes } from "react-router";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <Toaster />
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
