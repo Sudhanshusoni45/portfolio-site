c4953d21d8742d026214f3ec5c702710c3520c10
sudhanshu <sudhanshusoni45@gmail.com>
2024-03-01 17:42:24 +0530

    feat: absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths],
   build: {
     sourcemap: true, // Enable source maps for debugging.
     minify: "esbuild", // Use esbuild for minification.
